d04ac1cb0900ade4a02b95359b9c033844d2a0d0
deepsss98 <43499930+deepsss98@users.noreply.github.com>
2023-02-15 11:13:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const kural_display= async() => {
 try{
    let key_value=document.getElementById("search").value;
-   key_value>0 && key_value <=1330 ? "":alert("Invalid Value !!! Kindly enter number from 1 to 1330");
+   if(key_value>0 && key_value <=1330)
+   {
    const thirukural_URL=`https://api-thirukkural.vercel.app/api?num=${key_value}`;
    const response = await fetch(thirukural_URL);
    const result = await response.json(); //Converting raw data to readable stream
    display_jurisdictions_details(result,key_value);
+   }
+   else
+   {
+    document.getElementById("display-card").innerHTML="";
+    setTimeout(() => {
+      alert("Invalid Value !!! Kindly enter number from 1 to 1330");
+    }, 500);
+   }
 }
 catch(err){
     console.error(err.message);
@@ -15,7 +24,7 @@ catch(err){
 const display_jurisdictions_details=(kural,key_value)=>{
  let kurals_info="";
  kurals_info=`
-   <div class="card" id="display-card"  style="background-color:lightblue;">
+   <div class="card" id="display-card">
    <div class="card-header text-center text-dark fw-bold"> குறள்: ${key_value}</div>
    <div class="card-body">
    <h6 class="card-title text-center text-dark fw-bold">Chapter Name: ${kural.chap_tam}</h6>
@@ -32,7 +41,4 @@ const display_jurisdictions_details=(kural,key_value)=>{
 }
 
 let search_button=document.getElementById("search-btn")
-
 search_button.addEventListener("click",kural_display);
-
-
