a8d5da144998409ab53e3bd29054a09f71957f70
deepsss98 <43499930+deepsss98@users.noreply.github.com>
2023-02-15 11:02:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const kural_display= async() => {
+try{
+   let key_value=document.getElementById("search").value;
+   key_value>0 && key_value <=1330 ? "":alert("Invalid Value !!! Kindly enter number from 1 to 1330");
+   const thirukural_URL=`https://api-thirukkural.vercel.app/api?num=${key_value}`;
+   const response = await fetch(thirukural_URL);
+   const result = await response.json(); //Converting raw data to readable stream
+   display_jurisdictions_details(result,key_value);
+}
+catch(err){
+    console.error(err.message);
+}
+}
+
+const display_jurisdictions_details=(kural,key_value)=>{
+ let kurals_info="";
+ kurals_info=`
+   <div class="card" id="display-card"  style="background-color:lightblue;">
+   <div class="card-header text-center text-dark fw-bold"> குறள்: ${key_value}</div>
+   <div class="card-body">
+   <h6 class="card-title text-center text-dark fw-bold">Chapter Name: ${kural.chap_tam}</h6>
+   <h6 class="card-title fw-bold text-dark">குறள்:</h6>
+   <p class="card-text" id="kural-text">${kural.line1}<br>${kural.line2}</p>
+   <h6 class="card-title text-dark fw-bold">Tamil Explanation:</h6>
+   <p class="card-text">${kural.tam_exp}</p>
+   <h6 class="card-title text-dark fw-bold">English Explanation:</h6>
+   <p class="card-text">${kural.eng_exp}</p>
+   </div>
+   </div>
+   </div>`
+ document.getElementById("display-card").innerHTML=kurals_info;
+}
+
+let search_button=document.getElementById("search-btn")
+
+search_button.addEventListener("click",kural_display);
+
+
